8e36f71f9c1d683ddfcb7e66bc20bd7369e74848
AllieKim <allie0147@gmail.com>
2021-09-18 19:27:29 +0900

    [FEAT] Custom Error

---
+++ CityAirportSearch/Model/CustomError.swift
@@ -0,0 +1,11 @@
+//
+//  CustomError.swift
+//  CityAirportSearch
+//
+//  Created by Allie Kim on 2021/09/18.
+//
+
+import Foundation
+enum CustomError: Error {
+    case error(message: String)
+}

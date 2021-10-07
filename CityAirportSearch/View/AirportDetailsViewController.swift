a2f1eab3599735576b4f6a68d44484a40fc47e75
AllieKim <allie0147@gmail.com>
2021-10-07 23:52:27 +0900

    [FEAT] add AirportDetailViewController

---
+++ CityAirportSearch/View/AirportDetailsViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AirportDetailsViewController.swift
+//  CityAirportSearch
+//
+//  Created by Allie Kim on 2021/10/07.
+//
+
+import UIKit
+
+class AirportDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}

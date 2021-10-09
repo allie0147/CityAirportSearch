b430e1b337314a7bbe2eeaa0c19aab6d4313414d
AllieKim <allie0147@gmail.com>
2021-10-09 16:29:17 +0900

    [STYLE] AirportDetailsViewController

--- CityAirportSearch/View/AirportDetailsViewController.swift
+++ CityAirportSearch/View/AirportDetailsViewController.swift
@@ -6,9 +6,16 @@
 //
 
 import UIKit
+import MapKit
 
 class AirportDetailsViewController: UIViewController, Storyboardable {
 
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var airportNameLabel: UILabel!
+    @IBOutlet weak var distanceLabel: UILabel!
+    @IBOutlet weak var countryLabel: UILabel!
+    @IBOutlet weak var runwayLengthLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 

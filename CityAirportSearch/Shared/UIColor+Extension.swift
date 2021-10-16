01da9620f618862d801290232ee557bd19cbb540
AllieKim <allie0147@gmail.com>
2021-10-16 22:44:50 +0900

    [FEAT] UIColor Extension

---
+++ CityAirportSearch/Shared/UIColor+Extension.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+Extension.swift
+//  CityAirportSearch
+//
+//  Created by Allie Kim on 2021/10/16.
+//
+
+import UIKit
+
+extension UIColor {
+
+    @nonobjc class var reddishPink: UIColor {
+        return UIColor(red: 1.0, green: 45.0 / 255.0, blue: 85.0 / 255.0, alpha: 1.0)
+    }
+}
